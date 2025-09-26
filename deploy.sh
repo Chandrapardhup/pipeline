6ba20c4547c6bacd6141136f0ab13b82be160386
Chandrapardhup <chandrapardhup@gmail.com>
2025-09-26 10:20:38 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,16 @@
+ssh -o StrictHostKeyChecking=no -i ec2.pem ubuntu@65.1.64.5 "
+cat << 'EOF' > ~/deploy.sh
 #!/bin/bash
 cd /home/ubuntu/pipeline || git clone https://github.com/Chandrapardhup/pipeline.git && cd pipeline
 git pull
 sudo docker build -t pipeline-app .
 sudo docker stop pipeline-app || true && sudo docker rm pipeline-app || true
 sudo docker run -d -p 22:22 --name pipeline-app pipeline-app
-echo "$DOCKER_PASSWORD" | sudo docker login -u "$DOCKER_USERNAME" --password-stdin
-sudo docker tag pipeline-app:latest $DOCKER_USERNAME/pipeline-app:latest
-sudo docker push $DOCKER_USERNAME/pipeline-app:latest
+echo '${{ secrets.DOCKER_PASSWORD }}' | sudo docker login -u '${{ secrets.DOCKER_USERNAME }}' --password-stdin
+sudo docker tag pipeline-app:latest ${{ secrets.DOCKER_USERNAME }}/pipeline-app:latest
+sudo docker push ${{ secrets.DOCKER_USERNAME }}/pipeline-app:latest
+EOF
+chmod +x ~/deploy.sh
+bash ~/deploy.sh
+"
+
