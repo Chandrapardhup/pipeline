ee7f7df215d417c81318ed8e34c40e1fb415b6e9
Chandrapardhup <chandrapardhup@gmail.com>
2025-09-26 10:18:46 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ cd /home/ubuntu/pipeline || git clone https://github.com/Chandrapardhup/pipeline
 git pull
 sudo docker build -t pipeline-app .
 sudo docker stop pipeline-app || true && sudo docker rm pipeline-app || true
-sudo docker run -d -p 8080:80 --name pipeline-app pipeline-app
+sudo docker run -d -p 22:22 --name pipeline-app pipeline-app
 echo "$DOCKER_PASSWORD" | sudo docker login -u "$DOCKER_USERNAME" --password-stdin
 sudo docker tag pipeline-app:latest $DOCKER_USERNAME/pipeline-app:latest
 sudo docker push $DOCKER_USERNAME/pipeline-app:latest
