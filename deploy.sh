296622c3e4cf6449311a624be72133ea5dfefc4b
Chandrapardhup <chandrapardhup@gmail.com>
2025-09-26 10:25:59 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+
+# Go to repo folder or clone if missing
 cd /home/ubuntu/pipeline || git clone https://github.com/Chandrapardhup/pipeline.git && cd pipeline
 git pull
+
+# Build Docker image
 sudo docker build -t pipeline-app .
-sudo docker stop pipeline-app || true && sudo docker rm pipeline-app || true
+
+# Stop & remove old container
+sudo docker stop pipeline-app || true
+sudo docker rm pipeline-app || true
+
+# Run container (port 80 in container → 8080 on EC2)
 sudo docker run -d -p 8080:80 --name pipeline-app pipeline-app
+
+# Docker login & push
 echo "$DOCKER_PASSWORD" | sudo docker login -u "$DOCKER_USERNAME" --password-stdin
 sudo docker tag pipeline-app:latest $DOCKER_USERNAME/pipeline-app:latest
 sudo docker push $DOCKER_USERNAME/pipeline-app:latest
